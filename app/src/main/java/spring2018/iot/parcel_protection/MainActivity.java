6cfd54208a49a4ba3408ce0daf547ccc4808546e
arnabp <arnab.purkayastha@colorado.edu>
2018-05-04 16:57:53 -0600

    Bluetooth library added to initialization

--- app/src/main/java/spring2018/iot/parcel_protection/MainActivity.java
+++ app/src/main/java/spring2018/iot/parcel_protection/MainActivity.java
@@ -1,13 +1,59 @@
 package spring2018.iot.parcel_protection;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BluetoothSPP bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt = new BluetoothSPP(this);
+
+        if(!bt.isBluetoothAvailable()) {
+            Toast.makeText(getApplicationContext()
+                , "Bluetooth is not available"
+                , Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
+            public void onDataReceived(byte[] data, String message) {
+                Toast.makeText(getApplicationContext()
+                    , message + "\n"
+                    , Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    //stop this bluetooth service when App is destroyed.
+    public void onDestroy() {
+        super.onDestroy();
+        bt.stopService();
+    }
+
+    public void onStart() {
+        super.onStart();
+        //enable bluetooth and bt service.
+        if (!bt.isBluetoothEnabled()) {
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
+        } else {
+            if(!bt.isServiceAvailable()) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_ANDROID);
+            }
+        }
     }
 }
