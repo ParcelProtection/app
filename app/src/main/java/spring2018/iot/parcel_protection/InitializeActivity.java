a528bd992635b12dc792c8881438ed95ca92726a
arnabp <arnab.purkayastha@colorado.edu>
2018-05-05 15:49:19 -0600

    Moved ui files to the main project

---
+++ app/src/main/java/spring2018/iot/parcel_protection/InitializeActivity.java
@@ -0,0 +1,16 @@
+package spring2018.iot.parcel_protection;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by Arnab Purkayastha on 5/5/2018.
+ */
+
+public class InitializeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initialize);
+    }
+}
